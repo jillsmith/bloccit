ec8aba81078c3953f77710538d3ae8bd103e5b05
jillmsmith <jillma.smith@gmail.com>
2015-11-04 01:44:31 +0000

    Assignment 38

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
 
   resources :topics do
