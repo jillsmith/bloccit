cbc8dccd4dfca3d2aca4514d272b3bf9df32a517
jillmsmith <jillma.smith@gmail.com>
2015-11-04 18:52:13 +0000

    Assignment 39 Questions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
+    belongs_to :user
     belongs_to :topic
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
+    validates :user, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 include RandomData
 
+#See Users
+5.times do
+    user = User.create!(
+        name: RandomData.random_name,
+        email: RandomData.random_email,
+        password: RandomData.random_sentence
+    )
+end
+        
+
 #Seed Topics
 15.times do
    Topic.create!(
@@ -12,6 +22,7 @@ topics = Topic.all
 #Seed Post
 50.times do
     Post.create!(
+        user: users.sample,
         topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph)
@@ -61,7 +72,14 @@ questions = Question.all
     end
 sponsoredposts = SponsoredPost.all
 
+ user = User.first
+ user.update_attributes!(
+   email:"jillma.smith@gmail.com",
+   password: "helloworld"
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created" 
 puts "#{Comment.count} comments created"
